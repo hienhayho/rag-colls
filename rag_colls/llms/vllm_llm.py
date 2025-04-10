071e8399bb7e52314026bffdbe52caec4b551f1d
congtuong <tuongbck@gmail.com>
2025-04-10 16:34:39 +0000

    feat(llms): RCW-17 Add vLLM json format

--- rag_colls/llms/vllm_llm.py
+++ rag_colls/llms/vllm_llm.py
@@ -1,5 +1,9 @@
 import asyncio
+from typing import Type
+from pydantic import BaseModel
+from loguru import logger
 from vllm import LLM, SamplingParams
+from vllm.sampling_params import GuidedDecodingParams
 from rag_colls.core.base.llms.base import BaseCompletionLLM
 from rag_colls.types.llm import Message, LLMOutput, LLMUsage
 
@@ -39,6 +43,8 @@ class VLLM(BaseCompletionLLM):
             download_dir (str): The directory to download the model. Defaults to "model_cache".
         """
         kwargs["download_dir"] = download_dir
+        logger.info(f"Using VLLM with model: {model_name}")
+        logger.info(f"VLLM model download dir: {download_dir}")
         self.llm = LLM(
             model=model_name,
             trust_remote_code=trust_remote_code,
@@ -52,11 +58,21 @@ class VLLM(BaseCompletionLLM):
         )
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
 
+    def _is_support_json_output(self) -> bool:
+        """
+        Checks if the model supports JSON output.
+
+        Returns:
+            bool: True if the model supports JSON output, False otherwise.
+        """
+        return True
+
     def _complete(
         self,
         messages: list[Message],
         max_tokens: int = 512,
         temperature: float = 1,
+        response_format: Type[BaseModel] | None = None,
         top_p: int = 1,
         top_k: int = -1,
         **kwargs,
@@ -87,6 +103,12 @@ class VLLM(BaseCompletionLLM):
         kwargs = {
             k: v for k, v in kwargs.items() if k in SamplingParams.__struct_fields__
         }
+
+        if response_format:
+            kwargs["guided_decoding"] = GuidedDecodingParams(
+                json=response_format.model_json_schema(),
+            )
+
         sampling_params = SamplingParams(
             temperature=temperature,
             top_p=top_p,
@@ -116,6 +138,7 @@ class VLLM(BaseCompletionLLM):
         messages: list[Message],
         max_tokens: int = 512,
         temperature: float = 1,
+        response_format: Type[BaseModel] | None = None,
         top_p: int = 1,
         top_k: int = -1,
         **kwargs,
@@ -138,6 +161,7 @@ class VLLM(BaseCompletionLLM):
             messages,
             temperature=temperature,
             max_tokens=max_tokens,
+            response_format=response_format,
             top_p=top_p,
             top_k=top_k,
             **kwargs,
@@ -148,6 +172,7 @@ class VLLM(BaseCompletionLLM):
         messages: list[Message],
         max_tokens: int = 512,
         temperature: float = 1,
+        response_format: Type[BaseModel] | None = None,
         top_p: int = 1,
         top_k: int = -1,
         **kwargs,
@@ -170,6 +195,7 @@ class VLLM(BaseCompletionLLM):
             messages,
             temperature=temperature,
             max_tokens=max_tokens,
+            response_format=response_format,
             top_p=top_p,
             top_k=top_k,
             **kwargs,
@@ -180,6 +206,7 @@ class VLLM(BaseCompletionLLM):
         messages: list[Message],
         max_tokens: int = 512,
         temperature: float = 1,
+        response_format: Type[BaseModel] | None = None,
         top_p: int = 1,
         top_k: int = -1,
         **kwargs,
@@ -202,6 +229,7 @@ class VLLM(BaseCompletionLLM):
             messages,
             temperature=temperature,
             max_tokens=max_tokens,
+            response_format=response_format,
             top_p=top_p,
             top_k=top_k,
             **kwargs,
